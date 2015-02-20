00437fcd9657edb7cacd3d20555ed9ee61de0100
Marat Mavlyutov <mavlyutov@yandex-team.ru>
2015-02-20 13:26:16 +0300

    test fixed

--- openstack-plugin/src/test/java/jenkins/plugins/openstack/compute/JCloudsSlaveTemplateTest.java
+++ openstack-plugin/src/test/java/jenkins/plugins/openstack/compute/JCloudsSlaveTemplateTest.java
@@ -13,7 +13,7 @@ public class JCloudsSlaveTemplateTest extends HudsonTestCase {
     public void testConfigRoundtrip() throws Exception {
         String name = "testSlave";
         JCloudsSlaveTemplate originalTemplate = new JCloudsSlaveTemplate(name, "imageId", "hardwareId",
-                "openstack-slave-type1 openstack-type2", "userData", "1", false, null, null, true, 0, 0,
+                "openstack-slave-type1 openstack-type2", "userData", "1", false, null, null, true, 0,
                 "keyPair", "network1_id,network2_id", "default", null, JCloudsCloud.SlaveType.SSH);
 
         List<JCloudsSlaveTemplate> templates = new ArrayList<JCloudsSlaveTemplate>();
