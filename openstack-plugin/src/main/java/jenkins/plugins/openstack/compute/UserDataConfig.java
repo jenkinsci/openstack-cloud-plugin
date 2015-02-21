98c8bd6e4387159b4bd5e1cc345fb9a55f7a1690
Marat Mavlyutov <mavlyutov@yandex-team.ru>
2015-02-21 20:02:52 +0300

    get rid of nused code

--- openstack-plugin/src/main/java/jenkins/plugins/openstack/compute/UserDataConfig.java
+++ openstack-plugin/src/main/java/jenkins/plugins/openstack/compute/UserDataConfig.java
@@ -2,8 +2,6 @@ package jenkins.plugins.openstack.compute;
 
 
 import hudson.Extension;
-import java.util.ArrayList;
-import java.util.List;
 import org.jenkinsci.lib.configprovider.AbstractConfigProviderImpl;
 import org.jenkinsci.lib.configprovider.model.Config;
 import org.jenkinsci.lib.configprovider.model.ContentType;
@@ -11,37 +9,14 @@ import org.kohsuke.stapler.DataBoundConstructor;
 
 
 public class UserDataConfig extends Config {
-    public final List<Arg> args;
 
     @DataBoundConstructor
-    public UserDataConfig(String id, String name, String comment, String content, List<Arg> args) {
+    public UserDataConfig(String id, String name, String comment, String content) {
         super(id, name, comment, content);
-        if (args != null) {
-            List<Arg> filteredArgs = new ArrayList<UserDataConfig.Arg>();
-            for (Arg arg : args) {
-                if (arg.name != null && arg.name.trim().length() > 0) {
-                    filteredArgs.add(arg);
-                }
-            }
-            this.args = filteredArgs;
-        } else {
-            this.args = null;
-        }
-    }
-
-    public static class Arg {
-        public final String name;
-        @DataBoundConstructor
-        public Arg(final String name) {
-            this.name = name;
-        }
     }
 
     @Extension(ordinal = 70)
     public static class UserDataConfigProvider extends AbstractConfigProviderImpl {
-        public UserDataConfigProvider() {
-            load();
-        }
 
         @Override
         public ContentType getContentType() {
@@ -50,7 +25,7 @@ public class UserDataConfig extends Config {
 
         @Override
         public String getDisplayName() {
-            return "UserDataConfig";
+            return "Openstack User Data";
         }
 
     }
