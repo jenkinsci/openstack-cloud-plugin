0c550308ff1855fc6fd9ee238175bfb8da1b4a50
Oliver Gondža <ogondza@gmail.com>
2015-07-24 10:20:17 +0200

    Fix JCloudsSlaveTemplateTest

--- src/test/java/jenkins/plugins/openstack/compute/JCloudsSlaveTemplateTest.java
+++ src/test/java/jenkins/plugins/openstack/compute/JCloudsSlaveTemplateTest.java
@@ -5,31 +5,32 @@ import java.util.List;
 
 import org.jvnet.hudson.test.HudsonTestCase;
 
-/**
- * @author Vijay Kiran
- */
 public class JCloudsSlaveTemplateTest extends HudsonTestCase {
+    // Following will be null if can not be validated: imageId, hardwareId, networkId, availabilityZone
+    // TODO test userDataId, credentialsId
+    final String TEMPLATE_PROPERTIES = "name,labelString,numExecutors,stopOnTerminate,jvmOptions,fsRoot,installPrivateKey,overrideRetentionTime,keyPairName,securityGroups,slaveType";
+    final String CLOUD_PROPERTIES = "profile,identity,credential,endPointUrl,instanceCap,retentionTime,scriptTimeout,startTimeout,zone";
 
     public void testConfigRoundtrip() throws Exception {
-        String name = "testSlave";
-        JCloudsSlaveTemplate originalTemplate = new JCloudsSlaveTemplate(name, "imageId", "hardwareId",
+        final String TEMPLATE_NAME = "test-template";
+        final String CLOUD_NAME = "my-openstack";
+
+        JCloudsSlaveTemplate originalTemplate = new JCloudsSlaveTemplate(TEMPLATE_NAME, "imageId", "hardwareId",
                 "openstack-slave-type1 openstack-type2", "userData", "1", false, null, null, true, 0,
                 "keyPair", "network1_id,network2_id", "default", null, JCloudsCloud.SlaveType.SSH, null);
 
         List<JCloudsSlaveTemplate> templates = new ArrayList<JCloudsSlaveTemplate>();
         templates.add(originalTemplate);
 
-        JCloudsCloud originalCloud = new JCloudsCloud("aws-profile", "identity", "credential", "endPointUrl", 1, 30,
+        JCloudsCloud originalCloud = new JCloudsCloud(CLOUD_NAME, "identity", "credential", "endPointUrl", 1, 30,
                 600 * 1000, 600 * 1000, null, templates);
 
         hudson.clouds.add(originalCloud);
         submit(createWebClient().goTo("configure").getFormByName("config"));
 
-        assertEqualBeans(originalCloud, JCloudsCloud.getByName("openstack-profile"), "profile,identity,credential,privateKey,publicKey,endPointUrl");
+        final JCloudsCloud actualCloud = JCloudsCloud.getByName(CLOUD_NAME);
+        assertEqualBeans(originalCloud, actualCloud, CLOUD_PROPERTIES);
 
-        assertEqualBeans(originalTemplate, JCloudsCloud.getByName("aws-profile").getTemplate(name),
-                "name,labelString,description,numExecutors,stopOnTerminate");
-
+        assertEqualBeans(originalTemplate, actualCloud.getTemplate(TEMPLATE_NAME), TEMPLATE_PROPERTIES);
     }
-
 }
