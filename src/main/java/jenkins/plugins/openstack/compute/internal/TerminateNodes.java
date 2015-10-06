ee38148cf0e03f84f1013337343eea6223c926f2
Oliver Gondža <ogondza@gmail.com>
2015-10-06 08:06:34 +0200

    Remove leftovers from 85783a6da6efa21e6b15551893f7b5057454f732

--- src/main/java/jenkins/plugins/openstack/compute/internal/TerminateNodes.java
+++ src/main/java/jenkins/plugins/openstack/compute/internal/TerminateNodes.java
@@ -19,15 +19,12 @@ public class TerminateNodes implements Function<Iterable<RunningNode>, Void> {
     }
 
     public Void apply(Iterable<RunningNode> runningNode) {
-        Builder<String, String> cloudNodesToSuspendBuilder = ImmutableMultimap.<String, String>builder();
         Builder<String, String> cloudNodesToDestroyBuilder = ImmutableMultimap.<String, String>builder();
         for (RunningNode cloudTemplateNode : runningNode) {
             cloudNodesToDestroyBuilder.put(cloudTemplateNode.getCloudName(), cloudTemplateNode.getNode().getId());
         }
-        Multimap<String, String> cloudNodesToSuspend = cloudNodesToSuspendBuilder.build();
         Multimap<String, String> cloudNodesToDestroy = cloudNodesToDestroyBuilder.build();
 
-        suspendIfSupported(cloudNodesToSuspend);
         destroy(cloudNodesToDestroy);
         return null;
     }
@@ -39,21 +36,6 @@ public class TerminateNodes implements Function<Iterable<RunningNode>, Void> {
             Openstack os = JCloudsCloud.getByName(cloudToDestroy).getOpenstack();
             for (String node: nodesToDestroy) {
                 os.destroyServer(node);
-            }
-        }
-    }
-
-    private void suspendIfSupported(Multimap<String, String> cloudNodesToSuspend) {
-        for (String cloudToSuspend : cloudNodesToSuspend.keySet()) {
-            final Collection<String> nodesToSuspend = cloudNodesToSuspend.get(cloudToSuspend);
-            try {
-                listener.getLogger().println("Suspending nodes: " + nodesToSuspend);
-                Openstack os = JCloudsCloud.getByName(cloudToSuspend).getOpenstack();
-                for (String node: nodesToSuspend) {
-                    os.suspendServer(node);
-                }
-            } catch (UnsupportedOperationException e) {
-                listener.getLogger().println("Suspending unsupported on cloud: " + cloudToSuspend + "; nodes: " + nodesToSuspend + ": " + e);
             }
         }
     }
