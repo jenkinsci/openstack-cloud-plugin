279f5af9a8b59ba73ae72b93800094a5e7d27fbb
Oliver Gondža <ogondza@gmail.com>
2016-12-05 11:58:37 +0100

    Fixup f31adc6

--- src/main/java/jenkins/plugins/openstack/compute/SlaveOptionsDescriptor.java
+++ src/main/java/jenkins/plugins/openstack/compute/SlaveOptionsDescriptor.java
@@ -194,11 +194,10 @@ public final class SlaveOptionsDescriptor extends hudson.model.Descriptor<SlaveO
     ) {
         if (Util.fixEmpty(value) == null) {
             String d = getDefault(def, opts().getFloatingIpPool());
-            if (d != null) {
-                return FormValidation.ok(def(d));
-            }
-            return OK;
+            if (d != null) return FormValidation.ok(def(d));
+            // Not required
         }
+        return OK;
     }
 
     @Restricted(DoNotUse.class)
