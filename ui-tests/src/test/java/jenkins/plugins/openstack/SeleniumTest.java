ddebc6cac7e8d9aca2426d84ffb347902a578ea2
Oliver Gondža <ogondza@gmail.com>
2020-03-18 21:38:31 +0100

    Update Selenium test to integrate with openrc.sh

--- ui-tests/src/test/java/jenkins/plugins/openstack/SeleniumTest.java
+++ ui-tests/src/test/java/jenkins/plugins/openstack/SeleniumTest.java
@@ -26,12 +26,11 @@ package jenkins.plugins.openstack;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.startsWith;
 import static org.jenkinsci.test.acceptance.Matchers.*;
 import static org.jenkinsci.test.acceptance.po.FormValidation.Kind.OK;
 import static org.junit.Assert.assertEquals;
-
-import javax.inject.Named;
 
 import jenkins.plugins.openstack.po.OpenstackBuildWrapper;
 import jenkins.plugins.openstack.po.OpenstackCloud;
@@ -55,17 +54,15 @@ import org.jenkinsci.test.acceptance.po.Node;
 import org.jenkinsci.test.acceptance.po.Slave;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
 
-import com.google.inject.Inject;
-
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeThat;
 import static org.junit.Assume.assumeTrue;
 
 @WithPlugins("openstack-cloud")
-@TestActivation({"ENDPOINT", "CREDENTIAL"})
 public class SeleniumTest extends AbstractJUnitTest {
 
     private static final String CLOUD_INIT_NAME = "cloudInit";
@@ -77,40 +74,40 @@ public class SeleniumTest extends AbstractJUnitTest {
     // Must be absolute as ATM it is resolved relative to @WithCredentials
     private static final String SSH_KEY_PATH = "/jenkins/plugins/openstack/SeleniumTest/unsafe";
 
-    @Inject(optional = true) @Named("SeleniumTest.ENDPOINT")
-    public String ENDPOINT;
+    public static String OS_AUTH_URL = System.getenv("OS_AUTH_URL");
 
-    @Inject(optional = true) @Named("SeleniumTest.IDENTITY")
-    @Deprecated public String IDENTITY;
+    public static String OS_USERNAME = System.getenv("OS_USERNAME");
+    public static String OS_USER_DOMAIN_NAME = System.getenv("OS_USER_DOMAIN_NAME");
+    public static String OS_PROJECT_NAME = System.getenv("OS_PROJECT_NAME");
+    public static String OS_PROJECT_DOMAIN_NAME = System.getenv("OS_PROJECT_DOMAIN_NAME");
 
-    @Inject(optional = true) @Named("SeleniumTest.USER") public String USER;
-    @Inject(optional = true) @Named("SeleniumTest.USER_DOMAIN") public String USER_DOMAIN;
-    @Inject(optional = true) @Named("SeleniumTest.PROJECT") public String PROJECT;
-    @Inject(optional = true) @Named("SeleniumTest.PROJECT_DOMAIN") public String PROJECT_DOMAIN;
+    public static String OS_PASSWORD = System.getenv("OS_PASSWORD");
+    public static String OS_HARDWARE_ID = System.getenv("OS_HARDWARE_ID");
+    public static String OS_NETWORK_ID = System.getenv("OS_NETWORK_ID");
+    public static String OS_IMAGE_ID = System.getenv("OS_IMAGE_ID");
+    public static String OS_KEY_NAME = System.getenv("OS_KEY_NAME");
+    public static String OS_FIP_POOL_NAME = System.getenv("OS_FIP_POOL_NAME");
 
-    @Inject(optional = true) @Named("SeleniumTest.CREDENTIAL")
-    public String CREDENTIAL;
+    @BeforeClass
+    public static void statiSetUp() {
+        assumeThat("OS_AUTH_URL missing", OS_AUTH_URL, notNullValue());
 
-    @Inject(optional = true) @Named("SeleniumTest.HARDWARE_ID")
-    public String HARDWARE_ID;
+        assumeThat("OS_USERNAME missing", OS_USERNAME, notNullValue());
+        assumeThat("OS_USER_DOMAIN_NAME missing", OS_USER_DOMAIN_NAME, notNullValue());
+        assumeThat("OS_PROJECT_NAME missing", OS_PROJECT_NAME, notNullValue());
+        assumeThat("OS_PROJECT_DOMAIN_NAME missing", OS_PROJECT_DOMAIN_NAME, notNullValue());
 
-    @Inject(optional = true) @Named("SeleniumTest.NETWORK_ID")
-    public String NETWORK_ID;
-
-    @Inject(optional = true) @Named("SeleniumTest.IMAGE_ID")
-    public String IMAGE_ID;
-
-    @Inject(optional = true) @Named("SeleniumTest.KEY_PAIR_NAME")
-    public String KEY_PAIR_NAME;
-
-    @Inject(optional = true) @Named("SeleniumTest.FIP_POOL_NAME")
-    public String FIP_POOL_NAME;
+        assumeThat("OS_PASSWORD missing", OS_PASSWORD, notNullValue());
+        assumeThat("OS_HARDWARE_ID missing", OS_HARDWARE_ID, notNullValue());
+        assumeThat("OS_NETWORK_ID missing", OS_NETWORK_ID, notNullValue());
+        assumeThat("OS_IMAGE_ID missing", OS_IMAGE_ID, notNullValue());
+        assumeThat("OS_KEY_NAME missing", OS_KEY_NAME, notNullValue());
+    }
 
     @Before
     public void setUp() {
-        if ("".equals(USER_DOMAIN)) USER_DOMAIN = null;
-        if ("".equals(PROJECT_DOMAIN)) PROJECT_DOMAIN = null;
-        assertNull("IDENTITY field is deprecated, Use USER and PROJECT", IDENTITY);
+        if ("".equals(OS_USER_DOMAIN_NAME)) OS_USER_DOMAIN_NAME = null;
+        if ("".equals(OS_PROJECT_DOMAIN_NAME)) OS_PROJECT_DOMAIN_NAME = null;
     }
 
     @After // Terminate all nodes
@@ -136,7 +133,6 @@ public class SeleniumTest extends AbstractJUnitTest {
 
     @Test
     @WithCredentials(credentialType = WithCredentials.SSH_USERNAME_PRIVATE_KEY, values = {MACHINE_USERNAME, SSH_KEY_PATH}, id = SSH_CRED_ID)
-    @TestActivation({"HARDWARE_ID", "IMAGE_ID", "KEY_PAIR_NAME", "NETWORK_ID"})
     public void provisionSshSlave() {
         configureCloudInit("cloud-init");
         configureProvisioning("SSH", "label");
@@ -150,7 +146,6 @@ public class SeleniumTest extends AbstractJUnitTest {
 
     @Test
     @WithCredentials(credentialType = WithCredentials.USERNAME_PASSWORD, values = {MACHINE_USERNAME, "ath"}, id = SSH_CRED_ID)
-    @TestActivation({"HARDWARE_ID", "IMAGE_ID", "KEY_PAIR_NAME", "NETWORK_ID"})
     public void provisionSshSlaveWithPasswdAuth() {
         boolean isReachableFromOs = Boolean.getBoolean(System.getProperty("SeleniumTest.REACHABLE_FROM_OS"));
         assumeTrue("Skipping JNLP test as the test host is not reachable from OS. Set SeleniumTest.REACHABLE_FROM_OS in case it is.", isReachableFromOs);
@@ -167,7 +162,6 @@ public class SeleniumTest extends AbstractJUnitTest {
 
     @Test
     @WithCredentials(credentialType = WithCredentials.USERNAME_PASSWORD, values = {MACHINE_USERNAME, "ath"}, id = SSH_CRED_ID)
-    @TestActivation({"HARDWARE_ID", "IMAGE_ID", "KEY_PAIR_NAME", "NETWORK_ID"})
     public void provisionSshSlaveWithPasswdAuthRetryOnFailedAuth() {
         configureCloudInit("cloud-init-authfix");
         configureProvisioning("SSH", "label");
@@ -183,7 +177,6 @@ public class SeleniumTest extends AbstractJUnitTest {
     @Test
     // TODO: JENKINS-30784 Do not bother with credentials for jnlp slaves
     @WithCredentials(credentialType = WithCredentials.SSH_USERNAME_PRIVATE_KEY, values = {MACHINE_USERNAME, SSH_KEY_PATH}, id = SSH_CRED_ID)
-    @TestActivation({"HARDWARE_ID", "IMAGE_ID", "KEY_PAIR_NAME", "NETWORK_ID"})
     public void provisionJnlpSlave() {
         configureCloudInit("cloud-init-jnlp");
         configureProvisioning("JNLP", "label");
@@ -197,7 +190,6 @@ public class SeleniumTest extends AbstractJUnitTest {
 
     @Test @Issue("JENKINS-29998")
     @WithCredentials(credentialType = WithCredentials.SSH_USERNAME_PRIVATE_KEY, values = {MACHINE_USERNAME, SSH_KEY_PATH}, id = SSH_CRED_ID)
-    @TestActivation({"HARDWARE_ID", "IMAGE_ID", "KEY_PAIR_NAME", "NETWORK_ID"})
     @WithPlugins("matrix-project")
     public void scheduleMatrixWithoutLabel() {
         configureCloudInit("cloud-init");
@@ -218,7 +210,6 @@ public class SeleniumTest extends AbstractJUnitTest {
 
     @Test
     @WithCredentials(credentialType = WithCredentials.SSH_USERNAME_PRIVATE_KEY, values = {MACHINE_USERNAME, SSH_KEY_PATH}, id = SSH_CRED_ID)
-    @TestActivation({"HARDWARE_ID", "IMAGE_ID", "KEY_PAIR_NAME", "NETWORK_ID"})
     public void usePerBuildInstance() {
         configureCloudInit("cloud-init");
         configureProvisioning("SSH", "unused");
@@ -238,7 +229,6 @@ public class SeleniumTest extends AbstractJUnitTest {
 
     @Test
     @WithCredentials(credentialType = WithCredentials.SSH_USERNAME_PRIVATE_KEY, values = {MACHINE_USERNAME, SSH_KEY_PATH}, id = SSH_CRED_ID)
-    @TestActivation({"HARDWARE_ID", "IMAGE_ID", "KEY_PAIR_NAME", "NETWORK_ID"})
     public void useSingleUseSlave() {
         configureCloudInit("cloud-init");
         configureProvisioning("SSH", "label");
@@ -255,7 +245,6 @@ public class SeleniumTest extends AbstractJUnitTest {
 
     @Test
     @WithCredentials(credentialType = WithCredentials.USERNAME_PASSWORD, values = {MACHINE_USERNAME, "ath"}, id = SSH_CRED_ID)
-    @TestActivation({"HARDWARE_ID", "IMAGE_ID", "KEY_PAIR_NAME", "NETWORK_ID"})
     public void sshSlaveShouldSurviveRestart() {
         assumeTrue("This test requires a restartable Jenkins", jenkins.canRestart());
         configureCloudInit("cloud-init");
@@ -283,8 +272,8 @@ public class SeleniumTest extends AbstractJUnitTest {
     private OpenstackCloud addCloud(JenkinsConfig config) {
         return config.addCloud(OpenstackCloud.class)
                 .profile(CLOUD_NAME)
-                .endpoint(ENDPOINT)
-                .credential(USER, USER_DOMAIN, PROJECT, PROJECT_DOMAIN, CREDENTIAL)
+                .endpoint(OS_AUTH_URL)
+                .credential(OS_USERNAME, OS_USER_DOMAIN_NAME, OS_PROJECT_NAME, OS_PROJECT_DOMAIN_NAME, OS_PASSWORD)
         ;
     }
 
@@ -299,23 +288,23 @@ public class SeleniumTest extends AbstractJUnitTest {
     private void configureProvisioning(String type, String labels) {
         jenkins.configure();
         OpenstackCloud cloud = addCloud(jenkins.getConfigPage());
-        if (FIP_POOL_NAME != null) {
-            cloud.associateFloatingIp(FIP_POOL_NAME);
+        if (OS_FIP_POOL_NAME != null) {
+            cloud.associateFloatingIp(OS_FIP_POOL_NAME);
         }
         cloud.instanceCap(3);
         OpenstackSlaveTemplate template = cloud.addSlaveTemplate();
 
         template.name(CLOUD_DEFAULT_TEMPLATE);
         template.labels(labels);
-        template.hardwareId(HARDWARE_ID);
-        template.networkId(NETWORK_ID);
-        template.imageId(IMAGE_ID);
+        template.hardwareId(OS_HARDWARE_ID);
+        template.networkId(OS_NETWORK_ID);
+        template.imageId(OS_IMAGE_ID);
         template.connectionType(type);
         if ("SSH".equals(type)) {
             template.sshCredentials(SSH_CRED_ID);
         }
         template.userData(CLOUD_INIT_NAME);
-        template.keyPair(KEY_PAIR_NAME);
+        template.keyPair(OS_KEY_NAME);
         template.fsRoot("/tmp/jenkins");
         jenkins.save();
     }
